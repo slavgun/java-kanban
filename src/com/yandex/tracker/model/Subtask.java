a38ae23320e6317ca238995491cacd6212c9252b
slavgun <popov.slawka@yandex.ru>
2024-12-16 21:56:47 +0400

    Обновил тесты еще раз

--- src/com/yandex/tracker/model/Subtask.java
+++ src/com/yandex/tracker/model/Subtask.java
@@ -7,8 +7,12 @@ public class Subtask extends Task {
 
     public Subtask(String title, String description, TaskStatus status, int epicId) {
         super(title, description, status);
+        if (this.getId() == epicId) {
+            throw new IllegalArgumentException("Subtask cannot be its own epic.");
+        }
         this.epicId = epicId;
     }
+
 
     public int getEpicId() {
         return epicId;
