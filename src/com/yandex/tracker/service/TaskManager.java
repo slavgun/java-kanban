d87c24d901b77e5bcc3bfb19a4bee0e088c582c3
slavgun <popov.slawka@yandex.ru>
2024-12-16 14:49:01 +0400

    Добавлены изменения для sprint_6-solution

--- src/com/yandex/tracker/service/TaskManager.java
+++ src/com/yandex/tracker/service/TaskManager.java
@@ -3,24 +3,38 @@ package com.yandex.tracker.service;
 import com.yandex.tracker.model.Task;
 import com.yandex.tracker.model.Epic;
 import com.yandex.tracker.model.Subtask;
-
 import java.util.List;
 
 public interface TaskManager {
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubtask(Subtask subtask);
+
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
+
     List<Task> getAllTasks();
+
     List<Epic> getAllEpics();
+
     List<Subtask> getAllSubtasks();
+
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
-    void updateTask(Task task); // Добавлено
-    void updateSubtask(Subtask subtask); // Добавлено
-    List<Subtask> getSubtasksOfEpic(int epicId); // Добавлено
+
+    void updateTask(Task task);// Добавлено
+
+    void updateSubtask(Subtask subtask);// Добавлено
+
+    List<Subtask> getSubtasksOfEpic(int epicId);// Добавлено
+
     List<Task> getHistory();
 }
